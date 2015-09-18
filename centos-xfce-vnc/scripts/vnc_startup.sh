dc9f50c951a25baad88fc2ad8b15c2f5872c616f
toschneck <tobias.schneck@consol.de>
2015-09-18 13:58:41 +0200

    add `consol/centos-xfce-vnc` container

---
+++ centos-xfce-vnc/scripts/vnc_startup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#resolve_vnc_connection
+VNC_IP=$(ip addr show eth0 | grep -Po 'inet \K[\d.]+')
+VNC_PORT="590"${DISPLAY:1}
+
+##change vnc password
+echo "change vnc password!"
+(echo $VNC_PW && echo $VNC_PW) | vncpasswd
+
+##start vncserver and show logfile
+echo -e "\nStart VNCSERVER on DISPLAY= $DISPLAY \n\t=> connect via VNC viewer wiht $VNC_IP:$VNC_PORT"
+vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION
+/etc/xdg/xfce4/xinitrc &
+sleep 1
+
+for i in "$@"
+do
+case $i in
+    # if option `-t` or `--tail-log` block the execution and tail the VNC log
+    -t|--tail-log)
+    tail -f /root/.vnc/*$DISPLAY.log
+    ;;
+    *)
+    # unknown option ==> do nothing
+    ;;
+esac
+done
