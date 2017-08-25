d0925d225bba68bab2a3ce2611938aa1612b593c
Tobias Schneck <tobias.schneck@consol.de>
2017-08-25 14:39:56 +0200

    #27 add en_US.UTF-8 as default locale

--- src/ubuntu/install/tools.sh
+++ src/ubuntu/install/tools.sh
@@ -4,5 +4,8 @@ set -e
 
 echo "Install some common tools for further installation"
 apt-get update 
-apt-get install -y vim wget net-tools
+apt-get install -y vim wget net-tools locales
 apt-get clean -y
+
+echo "generate locales für en_US.UTF-8"
+locale-gen en_US.UTF-8
