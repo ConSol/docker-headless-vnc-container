d5a04de55574539517731341d116782b7e6807ee
Tobias Schneck <tobias.schneck@consol.de>
2017-09-05 09:59:33 +0200

    fix 35: chromium startup on centos

--- src/centos/install/chrome.sh
+++ src/centos/install/chrome.sh
@@ -9,5 +9,5 @@ ln -s /usr/bin/chromium-browser /usr/bin/google-chrome
 
 ### fix to start chromium in a Docker container, see https://github.com/ConSol/docker-headless-vnc-container/issues/2
 echo "CHROMIUM_FLAGS='--no-sandbox --start-maximized --user-data-dir'" > $HOME/.chromium-browser.init
-# add `source ~/install/chromium-wrapper`string before last line
-sed -i '$isource ~/install/chromium-wrapper' /usr/lib64/chromium-browser/chromium-browser.sh
+# add `source $INST_SCRIPTS/install/chromium-wrapper`string before last line
+sed -i '$isource $INST_SCRIPTS/chromium-wrapper' /usr/lib64/chromium-browser/chromium-browser.sh
