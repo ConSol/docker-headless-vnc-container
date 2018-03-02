c22b02ff673d647417457dcb9687e64f8e9c8dfb
Tobias Schneck <tobias.schneck@consol.de>
2018-03-02 12:57:46 +0100

    remove verbose output by default from set_user_permissions.sh

--- src/common/install/set_user_permission.sh
+++ src/common/install/set_user_permission.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 ### every exit != 0 fails the script
 set -e
+if [[ -z $DEBUG ]]; then
+    verbose="-v"
+fi
 
 for var in "$@"
 do
     echo "fix permissions for: $var"
-    find "$var"/ -name '*.sh' -exec chmod -v a+x {} +
-    find "$var"/ -name '*.desktop' -exec chmod -v a+x {} +
-    chgrp -R 0 "$var" && chmod -R -v a+rw "$var" && find "$var" -type d -exec chmod -v a+x {} +
+    find "$var"/ -name '*.sh' -exec chmod $verbose a+x {} +
+    find "$var"/ -name '*.desktop' -exec chmod $verbose a+x {} +
+    chgrp -R 0 "$var" && chmod -R $verbose a+rw "$var" && find "$var" -type d -exec chmod $verbose a+x {} +
 done
