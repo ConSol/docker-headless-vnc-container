924b055530e982c482759ef17aeb9de01c5f6016
Tobias Schneck <tobias.schneck@consol.de>
2018-03-26 18:51:40 +0200

    review PR #66: * change symbolic link to vnc_lite * websockify version back to still present problem if container has on startup no internet connection, see #50. * update date of REFRESHED_AT in Dockerfiles

--- src/common/install/no_vnc.sh
+++ src/common/install/no_vnc.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 ### every exit != 0 fails the script
 set -e
+set -u
 
 echo "Install noVNC - HTML5 based VNC viewer"
 mkdir -p $NO_VNC_HOME/utils/websockify
 wget -qO- https://github.com/novnc/noVNC/archive/v1.0.0.tar.gz | tar xz --strip 1 -C $NO_VNC_HOME
 # use older version of websockify to prevent hanging connections on offline containers, see https://github.com/ConSol/docker-headless-vnc-container/issues/50
-wget -qO- https://github.com/novnc/websockify/archive/v0.8.0.tar.gz | tar xz --strip 1 -C $NO_VNC_HOME/utils/websockify
+wget -qO- https://github.com/novnc/websockify/archive/v0.6.1.tar.gz | tar xz --strip 1 -C $NO_VNC_HOME/utils/websockify
 chmod +x -v $NO_VNC_HOME/utils/*.sh
 ## create index.html to forward automatically to `vnc_auto.html`
-ln -s $NO_VNC_HOME/vnc_auto.html $NO_VNC_HOME/index.html
+ln -s $NO_VNC_HOME/vnc_lite.html $NO_VNC_HOME/index.html
