811560167a8190f6772725bf436d6f1b2e0b4649
Tobias Schneck <tobias.schneck@consol.de>
2018-03-27 15:32:15 +0200

    add missing library for firefox on centos-icewm

--- src/centos/install/icewm_ui.sh
+++ src/centos/install/icewm_ui.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Install Xfce4 UI components and disable xfce-polkit"
 
-yum -y install xorg-x11-fonts* icewm
+yum -y install xorg-x11-fonts* xulrunner icewm
 yum -y groups install "Fonts"
 yum erase -y *power* *screensaver*
 yum clean all
