9bcad11e54e6376f27150812e6455713558464b2
Fabian Lim <fabianlim1989@gmail.com>
2018-07-13 23:12:49 +0800

    fix line breaks

--- src/ubuntu/install/chrome.sh
+++ src/ubuntu/install/chrome.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Install Chromium Browser"
-apt-get update
+apt-get update 
 apt-get install -y chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg
 apt-get clean -y
 ln -s /usr/bin/chromium-browser /usr/bin/google-chrome
--- src/ubuntu/install/tools.sh
+++ src/ubuntu/install/tools.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Install some common tools for further installation"
-apt-get update
+apt-get update 
 apt-get install -y vim wget net-tools locales bzip2 \
     python-numpy #used for websockify/novnc
 apt-get clean -y
