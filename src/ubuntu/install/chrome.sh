e90f0a89d194e7558aec3994a3eaf9b14657f84d
Fabian Lim <fabianlim1989@gmail.com>
2018-07-13 23:02:52 +0800

    revert ubuntu scripts

--- src/ubuntu/install/chrome.sh
+++ src/ubuntu/install/chrome.sh
@@ -4,8 +4,8 @@ set -e
 
 echo "Install Chromium Browser"
 apt-get update
-apt-get install -y chromium #chromium-l10n chromium-shell chromium-driver chromium-widevine
+apt-get install -y chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg
 apt-get clean -y
-ln -s /usr/bin/chromium /usr/bin/google-chrome
+ln -s /usr/bin/chromium-browser /usr/bin/google-chrome
 ### fix to start chromium in a Docker container, see https://github.com/ConSol/docker-headless-vnc-container/issues/2
 echo "CHROMIUM_FLAGS='--no-sandbox --start-maximized --user-data-dir'" > $HOME/.chromium-browser.init
--- src/ubuntu/install/tigervnc.sh
+++ src/ubuntu/install/tigervnc.sh
@@ -2,4 +2,4 @@
 set -e
 
 echo "Install TigerVNC server"
-wget --no-check-certificate -qO- https://dl.bintray.com/tigervnc/stable/tigervnc-1.8.0.x86_64.tar.gz | tar xz --strip 1 -C /
+wget -qO- https://dl.bintray.com/tigervnc/stable/tigervnc-1.8.0.x86_64.tar.gz | tar xz --strip 1 -C /
