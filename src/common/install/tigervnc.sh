e14609f2828bd7fc442bdfadf41e54593bdb5e52
Simon Hofmann <simon.hofmann@consol.de>
2019-01-03 17:18:21 +0100

    Both Ubuntu and CentOS now use a fixed version of tigervnc

---
+++ src/common/install/tigervnc.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Install TigerVNC server"
+wget -qO- https://dl.bintray.com/tigervnc/stable/tigervnc-1.8.0.x86_64.tar.gz | tar xz --strip 1 --no-same-owner --no-same-permissions -C /
