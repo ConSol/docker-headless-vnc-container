b3208220302411b3f6f19ee58652759285cff82c
Simon Hofmann <simon.hofmann@consol.de>
2019-01-24 19:36:44 +0100

    Restructured files to group novnc dependencies

--- src/centos/install/tools.sh
+++ src/centos/install/tools.sh
@@ -6,6 +6,5 @@ echo "Install some common tools for further installation"
 yum -y install epel-release 
 yum -y update
 yum -y install vim sudo wget which net-tools bzip2 \
-    psmisc \
-    numpy #used for websockify/novnc
+    psmisc
 yum clean all
--- src/ubuntu/install/tools.sh
+++ src/ubuntu/install/tools.sh
@@ -4,8 +4,7 @@ set -e
 
 echo "Install some common tools for further installation"
 apt-get update 
-apt-get install -y vim wget net-tools locales bzip2 \
-    python-numpy #used for websockify/novnc
+apt-get install -y vim wget net-tools locales bzip2
 apt-get clean -y
 
 echo "generate locales für en_US.UTF-8"
