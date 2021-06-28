788357a8e670f86d753bb26972d10150b31c2a23
Stefan Dirnstorfer <stefan@thetaris.com>
2021-06-28 14:18:44 +0200

    removed sandbox option

--- src/common/scripts/chrome-init.sh
+++ src/common/scripts/chrome-init.sh
@@ -8,4 +8,4 @@ VNC_RES_H=${VNC_RESOLUTION#*x}
 echo -e "\n------------------ update chromium-browser.init ------------------"
 echo -e "\n... set window size $VNC_RES_W x $VNC_RES_H as chrome window size!\n"
 
-echo "CHROMIUM_FLAGS='--no-sandbox --disable-gpu --user-data-dir --window-size=$VNC_RES_W,$VNC_RES_H --window-position=0,0'" > $HOME/.chromium-browser.init
+echo "CHROMIUM_FLAGS='--disable-gpu --user-data-dir --window-size=$VNC_RES_W,$VNC_RES_H --window-position=0,0'" > $HOME/.chromium-browser.init
--- src/ubuntu/install/chrome.sh
+++ src/ubuntu/install/chrome.sh
@@ -8,4 +8,4 @@ apt-get install -y chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg
 apt-get clean -y
 ln -s /usr/bin/chromium-browser /usr/bin/google-chrome
 ### fix to start chromium in a Docker container, see https://github.com/ConSol/docker-headless-vnc-container/issues/2
-echo "CHROMIUM_FLAGS='--no-sandbox --start-maximized --user-data-dir'" > $HOME/.chromium-browser.init
+echo "CHROMIUM_FLAGS='--start-maximized --user-data-dir'" > $HOME/.chromium-browser.init
