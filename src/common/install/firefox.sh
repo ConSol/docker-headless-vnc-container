087491d4a948d9e1c6e15dd4de9dd72bae3153d9
Stefan Dirnstorfer <stefan@thetaris.com>
2021-06-28 13:53:44 +0200

    Update noVnc and Firefox

--- src/common/install/firefox.sh
+++ src/common/install/firefox.sh
@@ -38,7 +38,7 @@ function instFF() {
     exit -1
 }
 
-instFF '45.9.0esr' '/usr/lib/firefox'
+instFF '89.0' '/usr/lib/firefox'
 
 #yum -y install firefox-45.7.0-2.el7.centos
 #yum -y install firefox
--- src/ubuntu/install/tigervnc.sh
+++ src/ubuntu/install/tigervnc.sh
@@ -2,4 +2,8 @@
 set -e
 
 echo "Install TigerVNC server"
-wget -qO- https://dl.bintray.com/tigervnc/stable/tigervnc-1.8.0.x86_64.tar.gz | tar xz --strip 1 -C /
+wget -qO- https://deac-fra.dl.sourceforge.net/project/tigervnc/stable/1.11.0/tigervnc-1.11.0.x86_64.tar.gz | tar xz --strip 1 -C /
+
+wget https://raw.githubusercontent.com/metalefty/tigervnc/master/unix/vncserver
+mv vncserver /usr/bin
+chmod ugo+x /usr/bin/vncserver
