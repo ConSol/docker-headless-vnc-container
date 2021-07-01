afccdfaa56906e94e893e6f776b5ca4d0608753d
Stefan Dirnstorfer <stefan@thetaris.com>
2021-07-01 13:06:42 +0200

    Fix prompt

--- src/install/set_user_permission.sh
+++ src/install/set_user_permission.sh
@@ -16,66 +16,8 @@ find "$HOME"/ -name '*.sh' -exec chmod $verbose a+x {} +
 find "$HOME"/ -name '*.desktop' -exec chmod $verbose a+x {} +
 chgrp -R 0 "$HOME" && chmod -R $verbose a+rw "$HOME" && find "$HOME" -type d -exec chmod $verbose a+x {} +
 
-cat > $HOME/.bashrc << EOF
-# ~/.bashrc: executed by bash(1) for non-login shells.
-# see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
-# for examples
-# If not running interactively, don't do anything
-case \$- in
-    *i*) ;;
-      *) return;;
-esac
-
-# history settings
-HISTCONTROL=ignoreboth
-shopt -s histappend
-HISTSIZE=1000
-HISTFILESIZE=2000
-
-shopt -s checkwinsize
-shopt -s globstar
-
-# enable color support of ls and also add handy aliases
-if [ -x /usr/bin/dircolors ]; then
-    test -r ~/.dircolors && eval "\$(dircolors -b ~/.dircolors)" || eval "\$(dircolors -b)"
-    alias ls='ls --color=auto'
-    #alias dir='dir --color=auto'
-    #alias vdir='vdir --color=auto'
-
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
-fi
-
-# colored GCC warnings and errors
-export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
-
-# some more ls aliases
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
-
-# Alias definitions.
-# You may want to put all your additions into a separate file like
-# ~/.bash_aliases, instead of adding them here directly.
-# See /usr/share/doc/bash-doc/examples in the bash-doc package.
-
-if [ -f ~/.bash_aliases ]; then
-    . ~/.bash_aliases
-fi
-
-# enable programmable completion features (you don't need to enable
-# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
-# sources /etc/bash.bashrc).
-if ! shopt -oq posix; then
-  if [ -f /usr/share/bash-completion/bash_completion ]; then
-    . /usr/share/bash-completion/bash_completion
-  elif [ -f /etc/bash_completion ]; then
-    . /etc/bash_completion
-  fi
-fi
-
-export PS1='\e[0;32m\u\e[m \e[0;36m\w\e[m \$ '
+cat >> $HOME/.bashrc << EOF
+export PS1='\[\e[0;32m\]\u \[\e[0;36m\]\w\[\e[m\] \$ '
 echo Welcome to Testup!
 echo Use \"sudo apt install xxx\" to install packages from the Ubuntu universe.
 echo
