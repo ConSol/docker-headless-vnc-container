eccaba9025cf621da2d6ea771127549db3226b81
chaoqing <chaoqingwang.nick@gmail.com>
2022-10-09 17:09:41 +0800

    upgrade ubuntu to 22.04;

--- src/common/install/no_vnc.sh
+++ src/common/install/no_vnc.sh
@@ -10,9 +10,9 @@ wget -qO- https://github.com/novnc/noVNC/archive/v1.0.0.tar.gz | tar xz --strip 
 wget -qO- https://github.com/novnc/websockify/archive/v0.6.1.tar.gz | tar xz --strip 1 -C $NO_VNC_HOME/utils/websockify
 chmod +x -v $NO_VNC_HOME/utils/*.sh
 ## create index.html to forward automatically to `vnc_lite.html`
-if [ -n NO_VNC_LITE ]; then
-	INDEX_FILE=vnc.html
-else
+if [ -n USE_VNC_LITE ]; then
 	INDEX_FILE=vnc_lite.html
+else
+	INDEX_FILE=vnc.html
 fi
 ln -s $NO_VNC_HOME/${INDEX_FILE} $NO_VNC_HOME/index.html
--- src/ubuntu/install/tools.sh
+++ src/ubuntu/install/tools.sh
@@ -4,12 +4,13 @@ set -e
 
 echo "Install some common tools for further installation"
 apt-get update 
-apt-get install -y sudo openssh-client vim wget net-tools locales bzip2 \
+apt-get install -y vim wget net-tools locales bzip2 \
+    sudo openssh-client ncdu unar bash-completion command-not-found \
+    zsh autojump \
     python-numpy #used for websockify/novnc
 apt-get clean -y
 
 echo "%sudo ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-echo "%dyno ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 echo "generate locales für en_US.UTF-8"
 locale-gen en_US.UTF-8
