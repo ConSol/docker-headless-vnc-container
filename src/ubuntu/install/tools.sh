fea4926fbda229ad427508af2f9384edc614bcd9
chaoqing <chaoqingwang.nick@gmail.com>
2022-10-13 21:13:14 +0800

    only ubuntu xfce maintained

--- src/ubuntu/install/tools.sh
+++ src/ubuntu/install/tools.sh
@@ -2,11 +2,13 @@
 ### every exit != 0 fails the script
 set -e
 
+echo y | unminimize
+
 echo "Install some common tools for further installation"
 apt-get update 
 apt-get install -y vim wget net-tools locales bzip2 \
     sudo openssh-client curl htop psmisc ncdu unar bash-completion command-not-found \
-    git zsh autojump less software-properties-common \
+    git zsh tmux autojump less software-properties-common \
     tigervnc-standalone-server tigervnc-viewer \
     python3-numpy #used for websockify/novnc
 apt-get clean -y
@@ -17,3 +19,21 @@ echo "default:default" | chpasswd
 
 echo "generate locales für en_US.UTF-8"
 locale-gen en_US.UTF-8
+
+### install firefox
+function install_firefox(){
+apt-get update
+apt install -y software-properties-common 
+add-apt-repository -y ppa:mozillateam/ppa
+# run `apt-cache policy` and inpect for following `Pin` section text
+# the Pin-Priority should be larger than the default ubuntu source
+cat > /etc/apt/preferences.d/mozillateam-ppa-pin-700 <<EOF
+Package: *
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 700
+EOF
+apt update
+apt install -y firefox
+}
+
+install_firefox
