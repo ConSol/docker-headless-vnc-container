ac7847a198f850d7883042b2b6183218c5de7efd
Stefan Dirnstorfer <stefan@thetaris.com>
2022-10-30 20:39:35 +0100

    suppress chrome update in kiosk mode

--- src/startup/start_chrome.sh
+++ src/startup/start_chrome.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # As chrome is already running, this is an equivalent of "open URL" function
-/usr/bin/google-chrome --disable-gpu --disable-dev-shm-usage --no-sandbox -test-type --disable-infobars
+/usr/bin/google-chrome --disable-gpu --disable-dev-shm-usage --no-sandbox -test-type --disable-infobars --simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT' --disable-component-update
