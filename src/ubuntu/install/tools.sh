3087262ca5d5f22bb06835ee96af4c25067feccd
orz <admin@uorz.me>
2022-11-26 13:14:54 +0000

    nss is not needed;

--- src/ubuntu/install/tools.sh
+++ src/ubuntu/install/tools.sh
@@ -10,6 +10,8 @@ apt-get install -y vim wget net-tools locales bzip2 \
     sudo openssh-client curl htop psmisc ncdu unar bash-completion command-not-found \
     git zsh tmux autojump less software-properties-common \
     tigervnc-standalone-server tigervnc-viewer \
+    python-is-python3 python3-venv build-essential zip make clang \
+    zlib1g zlib1g-dev libssl-dev libbz2-dev libsqlite3-dev \
     python3-numpy #used for websockify/novnc
 apt-get clean -y
 
