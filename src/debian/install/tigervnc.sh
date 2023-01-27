a01a9ed038fb52d3071f6e2bbe9b138706071503
Sven Nierlein <sven@nierlein.de>
2023-01-27 10:03:03 +0100

    fix tigervnc listening on localhost (fixes #165)
    port cannot be exposed by docker if the vnc server listens on localhost only.

--- src/debian/install/tigervnc.sh
+++ src/debian/install/tigervnc.sh
@@ -4,3 +4,4 @@ set -e
 echo "Install TigerVNC server"
 apt-get install -y tigervnc-standalone-server
 apt-get clean -y
+printf '\n# docker-headless-vnc-container:\n$localhost = "no";\n1;\n' >>/etc/tigervnc/vncserver-config-defaults
--- src/rocky/install/tigervnc.sh
+++ src/rocky/install/tigervnc.sh
@@ -4,3 +4,4 @@ set -e
 echo "Install TigerVNC server"
 dnf install -y tigervnc-server
 dnf clean all
+printf '\n# docker-headless-vnc-container:\nlocalhost=no\n' >>/etc/tigervnc/vncserver-config-defaults
