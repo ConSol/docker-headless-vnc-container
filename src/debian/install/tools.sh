e8f9d73a834514c186f6075c1ad2cbb6b9d49d00
Sven Nierlein <sven@nierlein.de>
2023-06-27 22:56:22 +0200

    fix generating locales (fixes #181)

--- src/debian/install/tools.sh
+++ src/debian/install/tools.sh
@@ -4,9 +4,10 @@ set -e
 
 echo "Install some common tools for further installation"
 apt-get update
-apt-get install -y vim wget net-tools locales bzip2 procps \
+apt-get install -y vim wget net-tools locales bzip2 procps apt-utils \
     python3-numpy #used for websockify/novnc
 apt-get clean -y
 
 echo "generate locales für en_US.UTF-8"
-locale-gen en_US.UTF-8
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
